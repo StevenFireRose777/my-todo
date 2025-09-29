2bcd3ba33ce671e4a0cbfb02df97928e6493cb1d
StevenFireRose777 <stevennnuno769@gmail.com>
2025-09-28 21:53:26 -0700

    added HTML/CSS properties for List

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1 className='todo-title'>List today's priorities!</h1>
+        <div id='todo-container'>
+          <input type="text" placeholder='Add task...' id="todo-input" maxLength={60}/>
+          <button id='todo-add'>Add</button>
+        </div>
+      </header>
+
+      <ul id='myUL'>
+        <li>Get a Job for summer 2026!</li>
+        <li>Join a club by next week!</li>
+        <li>Cry about College for an hour!</li>
+      </ul>
+
+      <button>Clear</button>
+      
+
+
     </>
   )
 }
